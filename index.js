603bfc6b27a9458677f9ea0c0c6b22ec3577ca08
Nikita <ovramenko.nikitka@gmail.com>
2025-05-21 08:07:11 -0400

    added play,pause,resume,getplaylist and get tracks functions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
-const port = 4002;
+const port = 4003;
 
 let ACCESS_TOKEN = null;
 let REFRESH_TOKEN = null;
@@ -15,13 +15,13 @@ const spotifyApi = new SpotifyWebApi({
     redirectUri: process.env.REDIRECT_URL
 });
 
-app.get('/login' ,async(req,res) => {
+app.get('/spotify/login' ,async(req,res) => {
     const scopes = ['user-read-private','user-read-email','user-read-playback-state','user-modify-playback-state','playlist-read-private'];
     res.redirect(spotifyApi.createAuthorizeURL(scopes));
     
 })
 
-app.get('/callback' , async(req,res) => {
+app.get('/spotify/callback' , async(req,res) => {
     console.log('Query received:', req.query);
     const error = req.query.error;
     const code = req.query.code;
@@ -47,7 +47,7 @@ app.get('/callback' , async(req,res) => {
 
 })
 
-app.get('/my-playlists', async (req, res) => {
+app.get('/spotify/my-playlists', async (req, res) => {
     try {
         if (!ACCESS_TOKEN) {
             return res.status(401).send('You must authenticate first');
@@ -56,7 +56,10 @@ app.get('/my-playlists', async (req, res) => {
         spotifyApi.setAccessToken(ACCESS_TOKEN);
 
         const data = await spotifyApi.getUserPlaylists();
-        const playlists = data.body.items;
+        const playlists = data.body.items.map(p => ({
+            name: p.name,
+            id: p.id
+        }));
 
         res.json(playlists);
     } catch (err) {
@@ -65,6 +68,47 @@ app.get('/my-playlists', async (req, res) => {
     }
 });
 
+
+app.get('/spotify/:playlist_id/tracks',async (req,res)=>{
+    spotifyApi.setAccessToken(ACCESS_TOKEN);
+    const data = await spotifyApi.getPlaylistTracks(req.params.playlist_id);
+    const tracks = data.body.items.map(item => ({
+        artists: item.track.artists,
+        songName: item.track.name,
+        uri: item.track.uri,
+        id: item.track.id
+    }) );
+
+    res.json(tracks);
+})
+
+
+app.get('/spotify/devices', async(req,res) =>{
+    spotifyApi.setAccessToken(ACCESS_TOKEN);
+    const data = spotifyApi.getMyDevices();
+    const devices = (await data).body;
+    res.json(devices);
+})
+
+app.get('/spotify/play/:id/:track',async (req,res)=> {
+
+    spotifyApi.setAccessToken(ACCESS_TOKEN);
+    await spotifyApi.play({device_id:`${req.params.id}`, uris:[`${req.params.track}`]});
+    res.send("Track started playing");
+})
+
+app.get('/spotify/play/:id',async (req,res)=> {
+
+    spotifyApi.setAccessToken(ACCESS_TOKEN);
+    await spotifyApi.play({device_id:`${req.params.id}`});
+    res.send("Track resume playing !");
+})
+
+app.get('/spotify/pause/:id',async (req,res) => {
+    spotifyApi.setAccessToken(ACCESS_TOKEN);
+    await spotifyApi.pause(device_id=`${req.params.id}`);
+
+})
 
 
 
